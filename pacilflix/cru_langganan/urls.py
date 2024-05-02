05427237a00192a3eed41683771f35576acd7374
raisahzr <raisa.hannaliya@gmail.com>
2024-05-02 11:21:42 +0700

    fix navigasi kelola -> beli langganan

--- pacilflix/cru_langganan/urls.py
+++ pacilflix/cru_langganan/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('kelola/', kelola_langganan, name='kelola_langganan'),
+    path('', kelola_langganan, name='kelola_langganan'),
     path('beli/', beli_langganan, name='beli_langganan'),
 ]
