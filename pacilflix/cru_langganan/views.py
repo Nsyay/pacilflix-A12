c28c8a4fe52f4b614f1a778a576170ddd15a1fad
raisahzr <raisa.hannaliya@gmail.com>
2024-05-02 08:46:40 +0700

    views, urls

--- pacilflix/cru_langganan/views.py
+++ pacilflix/cru_langganan/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def kelola_langganan(request):
+    return render(request, 'kelola_langganan.html')
+
+def beli_langganan(request):
+    return render(request, 'beli_langganan.html')
--- pacilflix/pacilflix/urls.py
+++ pacilflix/pacilflix/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('daftar-kontributor/', include('r_daftar_kontributor.urls')),
+    path('langganan/', include('cru_langganan.urls')),
 ]
