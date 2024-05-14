c93a19b796306dfccda591a08add2a5ea93cf064
wirr <prawirarzkn94@gmail.com>
2024-05-15 00:09:14 +0700

    Backend Read favorit dan unduhan

--- pacilflix/rd_daftar_unduhan/views.py
+++ pacilflix/rd_daftar_unduhan/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.db import connection
 
 # Create your views here.
 def daftar_unduhan(request):
-    return render(request, 'daftar_unduhan.html')
+    username = request.COOKIES.get('username')
+    records_daftar_unduhan = []
+
+    with connection.cursor() as cursor:
+        cursor.execute(
+            f'SELECT * FROM TAYANGAN_TERUNDUH WHERE username = %s', [username])
+        records_daftar_unduhan = cursor.fetchall()
+        for i in range(len(records_daftar_unduhan)):
+            cursor.execute(
+                f'SELECT judul FROM TAYANGAN WHERE id = \'{records_daftar_unduhan[i][0]}\'' )
+            records_daftar_unduhan[i] = records_daftar_unduhan[i] + cursor.fetchone()
+
+    context = {
+        'status': 'success',
+        'records_daftar_unduhan': records_daftar_unduhan,
+    }
+    response = render(request, 'daftar_unduhan.html', context)
+    return response
