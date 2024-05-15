14426615f3d5a0fe06c39f0658113e2e8b03bec3
Nasya Ayu <ayu.siti21@ui.ac.id>
2024-05-15 14:14:25 +0700

    change endpoint tayangan

--- pacilflix/tayangan/urls.py
+++ pacilflix/tayangan/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('episode/', episode, name='episode'),
     path('film/', film, name='film'),
     path('series/', series, name='series'),
-    path('tayangan/', tayangan, name='tayangan'),
+    path('', tayangan, name='tayangan'),
     path('trailer/', trailer, name='trailer'),
     path('trailer_guest/', trailer_guest, name='trailer_guest')
 ]
