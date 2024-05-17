0a64658b779c50846a0009633a900e6cf10e5dab
clarista <clarista05@gmail.com>
2024-05-17 14:19:31 +0700

    login regis success, trigger belom work

--- pacilflix/main/views.py
+++ pacilflix/main/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
 def show_main(request):
     return render(request, "main.html")
+
+def logout_view(request):
+    response = redirect('main:show_main')
+    for cookie in request.COOKIES:
+        response.delete_cookie(cookie)
+    return response
