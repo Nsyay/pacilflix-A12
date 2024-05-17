e3069cd5d69f7a0d2b163c91df8e2480c0ebc4aa
raisahzr <raisa.hannaliya@gmail.com>
2024-05-17 11:03:25 +0700

    daftar kontributor done

--- pacilflix/r_daftar_kontributor/views.py
+++ pacilflix/r_daftar_kontributor/views.py
@@ -1,7 +1,68 @@
 from django.shortcuts import render
+from django.db import connection
+from collections import defaultdict
 
-# Create your views here.
-app_name = 'daftar-kontributor'
+def get_kontributor_tipe():
+    contributors = defaultdict(lambda: {
+        'nama': '',
+        'jenis_kelamin': '',
+        'kewarganegaraan': '',
+        'kontribusi': []
+    })
+
+    with connection.cursor() as cursor:
+        query_contributors = """
+        SELECT id, nama, jenis_kelamin, kewarganegaraan
+        FROM CONTRIBUTORS
+        """
+        cursor.execute(query_contributors)
+        contributor_rows = cursor.fetchall()
+
+        for row in contributor_rows:
+            id, nama, jenis_kelamin, kewarganegaraan = row
+            contributors[id]['nama'] = nama
+            contributors[id]['jenis_kelamin'] = jenis_kelamin
+            contributors[id]['kewarganegaraan'] = kewarganegaraan
+
+        query_sutradara = """
+        SELECT id
+        FROM SUTRADARA
+        """
+        cursor.execute(query_sutradara)
+        sutradara_rows = cursor.fetchall()
+
+        for row in sutradara_rows:
+            id, = row
+            if id in contributors:
+                contributors[id]['kontribusi'].append('Sutradara')
+
+        query_penulis_skenario = """
+        SELECT id
+        FROM PENULIS_SKENARIO
+        """
+        cursor.execute(query_penulis_skenario)
+        penulis_skenario_rows = cursor.fetchall()
+
+        for row in penulis_skenario_rows:
+            id, = row
+            if id in contributors:
+                contributors[id]['kontribusi'].append('Penulis Skenario')
+
+        query_pemain = """
+        SELECT id
+        FROM PEMAIN
+        """
+        cursor.execute(query_pemain)
+        pemain_rows = cursor.fetchall()
+
+        for row in pemain_rows:
+            id, = row
+            if id in contributors:
+                contributors[id]['kontribusi'].append('Pemain')
+
+    contributors = dict(contributors)
+    return contributors
 
 def kontributor(request):
-    return render(request, 'kontributor.html')
+    contributors = get_kontributor_tipe()
+    return render(request, 'kontributor.html', {'contributors': contributors})
