9f3ffb6b87fb98576329e0668fc311efef1a9e92
Nasya Ayu <ayu.siti21@ui.ac.id>
2024-05-17 19:07:28 +0700

    ulasan done

--- pacilflix/ulasan/urls.py
+++ pacilflix/ulasan/urls.py
@@ -4,5 +4,6 @@ from .views import *
 app_name = 'ulasan'
 
 urlpatterns = [
-    path('tayangan/<id_tayangan>', ulasan, name='ulasan')
+    path('tayangan/<id_tayangan>', ulasan, name='ulasan'),
+    path('tayangan/<id_tayangan>', submit_ulasan, name='submit_ulasan')
 ]
--- pacilflix/ulasan/views.py
+++ pacilflix/ulasan/views.py
@@ -1,5 +1,6 @@
+import datetime
 from django.db import connection
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 # Create your views here.
 def ulasan(request, id_tayangan):
@@ -10,7 +11,7 @@ def ulasan(request, id_tayangan):
         ulasans = cursor.fetchall()
 
         cursor.execute(
-            f"SELECT judul FROM TAYANGAN WHERE id = '{id_tayangan}'")
+            f"SELECT id, judul FROM TAYANGAN WHERE id = '{id_tayangan}'")
         judul = cursor.fetchone()
 
     context = {
@@ -19,3 +20,20 @@ def ulasan(request, id_tayangan):
     }
     response = render(request, 'ulasan.html', context)
     return response
+
+def submit_ulasan(request, id_tayangan):
+    if request.method == 'POST':
+        username = request.COOKIES.get('username')
+        rating = request.POST.get('rating')
+        deskripsi = request.POST.get('deskripsi')
+        timestamp = datetime.datetime.now()
+
+        query = """
+        INSERT INTO ULASAN (id_tayangan, username, timestamp, rating, deskripsi)
+        VALUES (%s, %s, %s, %s, %s)
+        """
+        
+        with connection.cursor() as cursor:
+            cursor.execute(query, [id_tayangan, username, timestamp, rating, deskripsi])
+
+    return render(request, 'ulasan.html')
